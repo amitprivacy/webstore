2862ec3400a31a1b0ca46b77c7e10acaf0f94ded
Amit <amitsingh43713@gmail.com>
2020-07-24 21:47:54 +0530

    Adding project

---
+++ src/main/java/com/amit/webstore/config/DispatcherServletInitializer.java
@@ -0,0 +1,25 @@
+package com.amit.webstore.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		
+		return new Class[] {WebApplicationContextConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		
+		return new String[] {"/"};
+	}
+
+}
